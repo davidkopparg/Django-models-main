b661f72f3da3fa9402b4a760f2e0941b2d134011
davidkopparg <davidkopparg@gmail.com>
2023-02-26 10:11:51 -0300

    first commit

---
+++ marvel/e_commerce/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+
+# NOTE: Tenemos que importar los modelos con los que vamos a trabajar:
+from e_commerce.models import *
+
+# Register your models here.
+
+# NOTE: Aquí personalizamos los campos en el Django Admin.
+
+@admin.register(Comic)
+class ComicsAdmin(admin.ModelAdmin):
+    # NOTE: Para seleccionar los campos en la tabla de registros
+    list_display = ('id','marvel_id','title', 'stock_qty', 'price')
+
+    # NOTE: Filtro lateral de elementos:
+    list_filter= ('marvel_id','title')
+    
+    # NOTE: Buscador de elementos en la columna:
+    search_fields = ['title']
+
+    # NOTE: Para seleccionar los campos en el registro. 
+    #fields = ('marvel_id', 'title', 'stock_qty', 'price')
+
+    # NOTE: Genera un campo desplegable con los registros seleccionados.
+    fieldsets = (
+        (None, {
+            'fields': ('marvel_id', 'title', 'stock_qty')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('description','price', 'picture'),
+        }),
+    )
+    
+@admin.register(WishList)
+class WishListAdmin(admin.ModelAdmin):
+    # NOTE: Para seleccionar los campos en la tabla de registros
+     list_display = ('id','user','comic', 'favorite', 'cart','wished_qty','bought_qty')
+    #list_display = ('user', 'comic', 'favorite', 'wished_qty')
